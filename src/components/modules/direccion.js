09e7f72e1703ae811009dc2dcd2022845cc866ef
ricardcutzh <ricardcutzh@gmail.com>
2019-05-03 22:25:24 -0600

    agregando host

--- src/components/modules/direccion.js
+++ src/components/modules/direccion.js
@@ -2,5 +2,5 @@
 export default function getIp() {
     //return 'http://192.168.0.101:8000/';
     //return 'http://192.168.43.189:8000/';
-    return 'http://192.168.0.3:8000/'; 
+    return 'http://back:8000/'; 
 }
