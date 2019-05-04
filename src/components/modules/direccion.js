8ef5c04060174503d80dc19798b5200b1f484d1c
ricardcutzh <ricardcutzh@gmail.com>
2019-05-04 07:00:26 -0600

    ultimoas configuraciones

--- src/components/modules/direccion.js
+++ src/components/modules/direccion.js
@@ -2,5 +2,5 @@
 export default function getIp() {
     //return 'http://192.168.0.101:8000/';
     //return 'http://192.168.43.189:8000/';
-    return 'http://34.66.107.116:8000'; 
+    return 'http://34.66.107.116:8000/'; 
 }
